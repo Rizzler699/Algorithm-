9ec1dfdc482ce4e3a3056e603933c4cb24299d91
Rishit Grover <rishitgrover80@gmail.com>
2026-02-01 00:56:39 +0530

    Refactor linear search implementation and timing

--- Lab1/linearsearch.cpp
+++ Lab1/linearsearch.cpp
@@ -3,7 +3,6 @@
 #include <cstdlib>
 using namespace std;
 
-// Linear Search Function
 int linearSearch(int arr[], int n, int key)
 {
     for (int i = 0; i < n; i++) {
@@ -17,22 +16,19 @@ int main() {
     srand(time(0));
 
     cout << "InputSize\tAverageTime(seconds)\n";
-
-    // Input sizes from 1000 to 20000
+    
     for (int n = 1000; n <= 20000; n += 1000) {
 
         double totalTime = 0.0;
 
-        // Repeat 100 times
-        for (int r = 0; r < 100; r++) {
+        for (int k = 0; k < 100; k++) {
 
             int *arr = new int[n];
 
-            // Fill array with random numbers
             for (int i = 0; i < n; i++)
                 arr[i] = rand();
 
-            int key = arr[n - 1];  // Worst-case search
+            int key = arr[n - 1]; 
 
             clock_t start = clock();
             linearSearch(arr, n, key);
@@ -47,3 +43,4 @@ int main() {
 
     return 0;
 }
+
