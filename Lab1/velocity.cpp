073a7e68c572c5f69f0b96e874be81dcaeb81659
Rishit Grover <rishitgrover80@gmail.com>
2026-02-01 00:55:33 +0530

    Rename findTips to NumDips and update prompts

--- Lab1/velocity.cpp
+++ Lab1/velocity.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 using namespace std;
 
-// Given recursive function
-int findTips(double v)
+int NumDips(double v)
 {
     if (v < 1.0)
         return 0;
 
-    return 1 + findTips(v - 0.425*v);
+    return 1 + NumDips(v - 0.425*v);
 }
 
 int main() {
 int v;
-   cout<<"Enter the velocity:";
+   cout<<"Enter the velocity of the ball:";
    cin>>v;
-          cout<<  findTips(v);
-         
-    
-
+          cout<<  NumDips(v);
     return 0;
-
 }
